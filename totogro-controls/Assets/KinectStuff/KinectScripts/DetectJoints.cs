041f0528851b0133aa5d13c91d3984550b963741
Shaun Chiang <redbeansodapop@hotmail.com>
2017-01-12 16:31:01 -0500

    Added totogro-controls
    Testbed for kinect controls and gesture recognition.

---
+++ totogro-controls/Assets/KinectStuff/KinectScripts/DetectJoints.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+//using System.;
+
+public class DetectJoints : MonoBehaviour {
+
+    public GameObject BodySrcMgr;
+    public JointType TrackedJoint;
+    private BodySourceManager bodyManager;
+    private Body[] bodies;
+    private float currentclosest;
+
+    //static string HandState;
+
+    // Use this for initialization
+    void Start () {
+        if (BodySrcMgr == null)
+        {
+            Debug.Log("Assign game object with BodySrcMgr");
+        }
+        else
+        {
+            bodyManager = BodySrcMgr.GetComponent<BodySourceManager>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (bodyManager == null)
+        {
+            return;
+        }
+        bodies = bodyManager.GetData();
+        if (bodies == null)
+        {
+            return;
+        }
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+                var pos = body.Joints[TrackedJoint].Position;
+                if (pos.Z < currentclosest)
+                {
+                    pos.Z = currentclosest;
+                }
+                gameObject.transform.position = new Vector3(pos.X, pos.Y, pos.Z);  // commented out to find closest body
+            }
+        }
+        /*
+        //warning: stupid code ahead
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+                var pos = body.Joints[TrackedJoint].Position;
+                if (Mathf.Abs(pos.Z - currentclosest) < .5)
+                {
+                    gameObject.transform.position = new Vector3(pos.X, pos.Y, pos.Z);  // commented out to find closest body
+                }
+            }
+        }
+        currentclosest = 10000f;
+        //end of stupid code
+        */
+
+        //HandState = bodies[0].HandRightState.ToString();
+    }
+
+    public string Hand1State () {
+        foreach (var body in bodies)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+                return bodies[0].HandRightState.ToString();
+            }
+        }
+        return "No Bodies found...";
+    }
+}
---
+++ totogro-controls/Assets/MovementDetector.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementDetector : MonoBehaviour {
+
+    public Text infoText;
+    private GameObject leftHand;
+    private GameObject rightHand;
+    private GameObject leftHip;
+    private Rigidbody lhandrb;
+    private Rigidbody rhandrb;
+    private Rigidbody lhiprb;
+    private Vector3 lhandprevious;
+    private Vector3 lhipprevious;
+    private Vector3 rhandprevious;
+    private Vector3 lhandcurrent;
+    private Vector3 lhipcurrent;
+    private Vector3 rhandcurrent;
+    private Vector3 lhandvel;
+    private Vector3 rhandvel;
+    private Vector3 lhipvel;
+
+
+    private void Start()
+    {
+        leftHand = GameObject.FindWithTag("LeftHand");
+        lhandrb = leftHand.GetComponent<Rigidbody>();
+        leftHip = GameObject.FindWithTag("LeftHip");
+        lhiprb = leftHip.GetComponent<Rigidbody>();
+        rightHand = GameObject.FindWithTag("RightHand");
+        rhandrb = rightHand.GetComponent<Rigidbody>();
+        lhandprevious = lhandrb.transform.position;
+        rhandprevious = rhandrb.transform.position;
+        lhipprevious = lhiprb.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        lhandcurrent = lhandrb.transform.position;
+        rhandcurrent = rhandrb.transform.position;
+        lhipprevious = lhiprb.transform.position;
+        lhandvel = (lhandcurrent - lhandprevious) / Time.deltaTime;
+        rhandvel = (rhandcurrent - rhandprevious) / Time.deltaTime;
+        lhipvel = (lhipcurrent - lhipprevious) / Time.deltaTime;
+
+        infoText.text = "Movements: ";
+        if ((lhandvel.y>1)&&(rhandvel.y>1))
+        {
+            infoText.text += "Grow Up ";
+            Debug.Log("Grow Up");
+        }
+        if (lhipvel.y>1)
+        {
+            infoText.text += "Jump ";
+            Debug.Log("Jump");
+        }
+        if ((lhandvel.x > 1) && (rhandvel.x > 1))
+        {
+            infoText.text += "Grow Sides";
+            Debug.Log("Grow Sides");
+        }
+        Debug.Log("left Hand: "+lhandrb.velocity.y);
+        lhandprevious = lhandrb.transform.position;
+        rhandprevious = rhandrb.transform.position;
+        lhipprevious = lhiprb.transform.position;
+
+    }
+
+
+}
